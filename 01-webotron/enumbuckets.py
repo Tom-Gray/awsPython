9367d015cda11323f3f31ab63131a5a023f5a3d7
tom.gray <tom.gray@tribalgroup.com>
2019-02-03 20:02:09 +1100

    build out bucket creation

--- 01-webotron/enumbuckets.py
+++
@@ -1,6 +0,0 @@
-import boto3
-session = boto3.Session(profile_name='pythonaws')
-s3 = session.resource('s3')
-
-for bucket in s3.buckets.all():
-    print(bucket)
--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,6 +1,7 @@
 import boto3
 import sys
 import click
+from botocore.exceptions import ClientError
 
 print(sys.argv)
 session = boto3.Session(profile_name='pythonaws')
@@ -13,7 +14,7 @@ def cli():
 
 @cli.command('list-buckets')
 def list_buckets():
-    "List all s3 buckets"
+    "List all s3 buckets!"
     for bucket in s3.buckets.all():
         print(bucket)
 
@@ -23,6 +24,45 @@ def list_bucket_objects(bucket):
     "List objects in S3 bucket"
     for obj in s3.Bucket(bucket).objects.all():
         print(obj)
+
+@cli.command('setup-bucket')
+@click.argument('bucket')
+def setup_bucket(bucket):
+    "Create and configure S3 website bucket"
+    new_bucket = None
+    try:
+        new_bucket = s3.create_bucket(Bucket=bucket, CreateBucketConfiguration={'LocationConstraint': session.region_name})
+    except ClientError as e:
+        if e.response['Error']['Code'] == "BucketAlreadyOwnedByYou":
+            new_bucket = s3.Bucket(bucket)
+        else:
+            raise e
+    policy = """
+    {
+    "Version":"2012-10-17",
+    "Statement":[{
+        "Sid":"PublicReadGetObject",
+            "Effect":"Allow",
+        "Principal": "*",
+        "Action":["s3:GetObject"],
+        "Resource":["arn:aws:s3:::%s/*"
+        ]
+        }
+    ]
+    }
+    """ % new_bucket.name
+
+    policy = policy.strip()
+    pol = new_bucket.Policy()
+    pol.put(Policy=policy)
+    ws = new_bucket.Website()
+    ws.put(WebsiteConfiguration={'ErrorDocument': {
+                'Key': 'error.html'
+            },
+            'IndexDocument': {
+                'Suffix': 'index.html'
+            }})
+    return
 
 
 if __name__ == '__main__':
