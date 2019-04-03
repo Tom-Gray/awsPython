a2432476dcc0f2ae6f79233aeaa862e948fc2bdf
Tom Gray <tomgray@MacBook-Pro-7.local>
2019-04-04 10:25:01 +1100

    work so far

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -1,4 +1,19 @@
+import urllib
+import boto3
+
+
+def start_label_detection(bucket, key):
+    rekognition_client = boto3.client('rekognition')
+    response = rekognition_client.start_label_detection(
+        Video={'S3Object': {'Bucket': bucket, 'Name': key}})
+
+    print(response)
+
+
 def start_processing_video(event, context):
     print(event)
-
+    for record in evenrt['Records']:
+        bucket = record['s3']['bucket']['name']
+        key = urllib.parse.unquote_plus(record['s3']['object']['key'])
+        start_label_detection(bucket, key)
     return
